e898c78cb21d6086b846263ecc62cd26f067741c
constant <sukury47@gmail.com>
2021-05-11 11:13:35 +0900

    Add base code for listChangeLister

--- src/main/kotlin/com/github/sukury47/leavemealone/models/UglyBinary.kt
+++ src/main/kotlin/com/github/sukury47/leavemealone/models/UglyBinary.kt
@@ -2,8 +2,23 @@ package com.github.sukury47.leavemealone.models
 
 import javafx.beans.property.IntegerProperty
 import javafx.beans.property.SimpleIntegerProperty
+import javafx.beans.property.SimpleStringProperty
+import java.util.concurrent.atomic.AtomicInteger
 
-class UglyBinary {
+class UglyBinary(name: String) {
+    val id: Number = sequence()
+    val nameProperty = SimpleStringProperty()
     val widthProperty = SimpleIntegerProperty()
     val heightProperty = SimpleIntegerProperty()
+
+    init {
+        nameProperty.value = name
+    }
+
+    companion object {
+        private var id = AtomicInteger(1)
+        fun sequence() : Number = id.getAndIncrement()
+    }
+
+
 }
--- src/main/kotlin/com/github/sukury47/leavemealone/viewmodels/VMRoot.kt
+++ src/main/kotlin/com/github/sukury47/leavemealone/viewmodels/VMRoot.kt
@@ -1,5 +1,9 @@
 package com.github.sukury47.leavemealone.viewmodels
 
-class VMRoot {
+import com.github.sukury47.leavemealone.models.UglyBinary
+import javafx.beans.Observable
+import javafx.collections.FXCollections
 
+class VMRoot {
+    val uglyBinariesProperty = FXCollections.observableArrayList<UglyBinary> { item -> arrayOf(item.nameProperty, item.widthProperty, item.heightProperty) }
 }
